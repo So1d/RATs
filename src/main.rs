bb367dcc54db1cbefa0ef501ad32c9df2d763b48
Luis Dutra <machadodt@tutamail.com>
2025-10-05 03:07:54 -0300

    Some fix and code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,44 @@
 use i3ipc::{
-    event::{inner::WindowChange, Event},
     I3Connection, I3EventListener, Subscription,
+    event::{Event, inner::WindowChange},
 };
 use rats::{self, FocusedWindow};
 
 fn main() {
-    let mut listener = I3EventListener::connect().unwrap();
-    let mut connected = I3Connection::connect().unwrap();
+    let mut listener = I3EventListener::connect().expect("Failed to connect event listener on wm");
+    let mut connected = I3Connection::connect().expect("Failed to set connection with wm");
 
     listener.subscribe(&[Subscription::Window]).unwrap();
     println!("Hearing events...");
 
     for event in listener.listen() {
-        match event.unwrap() {
-            Event::WindowEvent(eve) => match eve.change {
-                WindowChange::New => {
-                    println!("Sei la porra, mas acho que tem janela nova ae");
-                    let tree = connected.get_tree().expect("Failed do get tree");
+        if let Ok(Event::WindowEvent(eve)) = event {
+            if eve.change == WindowChange::New {
+                println!("New window has detected");
+                let tree = connected.get_tree().expect("Failed do get tree");
 
-                    match rats::find_focused(&tree) {
-                        FocusedWindow::Here(width, height) => {
-                            println!("Passamo do find focused");
-                            println!("width: {} height: {}", width, height);
-                            if width == 0 {
-                                println!("Problemas mesmo no width")
-                            };
-                            if height == 0 {
-                                println!("Problemas mesmo no height")
-                            };
-                            if width < height {
-                                connected
-                                    .run_command("split v")
-                                    .expect("Falha ao splitar v");
-                                println!("splitamo v");
-                            } else if height < width {
-                                connected
-                                    .run_command("split h")
-                                    .expect("Falha ao splitar h");
-                                println!("splitamo h");
-                            } else if height == width {
-                                connected
-                                    .run_command("split h")
-                                    .expect("Falha ao splitar h");
-                                println!("splitamo h");
-                            };
-                        }
-
-                        FocusedWindow::NoOne => {
+                match rats::find_focused(&tree) {
+                    FocusedWindow::Here(width, height) => {
+                        if width < height {
                             connected
-                                .run_command("nop")
-                                .expect("Nemhuma janela encontrada");
+                                .run_command("split v")
+                                .expect("Falha ao splitar v");
+                            println!("splitamo v");
+                        } else {
+                            connected
+                                .run_command("split h")
+                                .expect("Falha ao splitar h");
+                            println!("splitamo h");
                         }
-                    };
+                    }
+
+                    FocusedWindow::NoOne => {
+                        connected
+                            .run_command("nop")
+                            .expect("Nemhuma janela encontrada");
+                    }
                 }
-                _ => {}
-            },
-            _ => {}
+            }
         }
     }
 }
