c0351078dae8a727d5914210cc78fe9b40ba98b9
Luis Dutra <machadodt@tutamail.com>
2025-10-05 02:27:53 -0300

    Using lib now

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use i3ipc::I3Connection;
-use i3ipc::event::inner::WindowChange;
-use i3ipc::reply;
-use i3ipc::{I3EventListener, Subscription, event::Event};
-
-enum FocusedWindow {
-    Here(i32, i32),
-    NoOne,
-}
+use i3ipc::{
+    event::{inner::WindowChange, Event},
+    I3Connection, I3EventListener, Subscription,
+};
+use rats::{self, FocusedWindow};
 
 fn main() {
     let mut listener = I3EventListener::connect().unwrap();
@@ -22,7 +18,7 @@ fn main() {
                     println!("Sei la porra, mas acho que tem janela nova ae");
                     let tree = connected.get_tree().expect("Failed do get tree");
 
-                    match find_focused(&tree) {
+                    match rats::find_focused(&tree) {
                         FocusedWindow::Here(width, height) => {
                             println!("Passamo do find focused");
                             println!("width: {} height: {}", width, height);
@@ -42,9 +38,12 @@ fn main() {
                                     .run_command("split h")
                                     .expect("Falha ao splitar h");
                                 println!("splitamo h");
-                            } else {
-                                println!("Algo deu errado");
-                            }
+                            } else if height == width {
+                                connected
+                                    .run_command("split h")
+                                    .expect("Falha ao splitar h");
+                                println!("splitamo h");
+                            };
                         }
 
                         FocusedWindow::NoOne => {
@@ -59,19 +58,4 @@ fn main() {
             _ => {}
         }
     }
-}
-fn find_focused(tree: &reply::Node) -> FocusedWindow {
-    if tree.focused {
-        println!("Certamente encontramos a janela focada");
-        let window = tree.window_rect;
-        return FocusedWindow::Here(window.2, window.3);
-    } else {
-        for node in &tree.nodes {
-            if let FocusedWindow::Here(width, height) = find_focused(&node) {
-                return FocusedWindow::Here(width, height);
-            }
-        }
-    }
-    println!("Ou deu merda ou nao achamo nemhuma focada");
-    FocusedWindow::NoOne
 }
